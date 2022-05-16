3c03bb7a65da7063d4aa620b516f1b0803d7ad83
Tommaso Buvoli <tommasosignup@gmail.com>
2022-05-15 19:45:33 -0700

    Updated LinearlyImplicitExponentialIntegratorConst.m : switched mode ordering

--- Integrators/ConstantTimestep/Abstract/Types/LinearlyImplicitExponentialIntegratorConst.m
+++ Integrators/ConstantTimestep/Abstract/Types/LinearlyImplicitExponentialIntegratorConst.m
@@ -3,8 +3,8 @@ classdef LinearlyImplicitExponentialIntegratorConst < IntegratorConst
     properties
         linear_solver
         phi_evaluator
-        % mode = 1 -> f^{1} exponential, f^{2} linearly implicit
-        % mode = 2 -> f^{1} linearly implicit, f^{2} exponential
+        % mode = 1 -> f^{1} linearly implicit, f^{2} exponential
+        % mode = 2 -> f^{1} exponential, f^{2} linearly implicit
         mode    
     end
     
@@ -25,41 +25,41 @@ classdef LinearlyImplicitExponentialIntegratorConst < IntegratorConst
             this.linear_solver = options.linear_solver;
             this.mode = 1;
         end
-                
-        function J = RHSexp(this, problem, u)
-            switch(this.mode)
-                case 1
-                    J = problem.RHS(u, 1);
-                case 2
-                    J = problem.RHS(u, 2);
-            end
-        end
-        
+           
         function J = RHSli(this, problem, u)
             switch(this.mode)
                 case 1
+                    J = problem.RHS(u, 1);
+                case 2
+                    J = problem.RHS(u, 2);
+            end  
+        end
+        
+        function J = RHSexp(this, problem, u)
+            switch(this.mode)
+                case 1
                     J = problem.RHS(u, 2);
                 case 2
                     J = problem.RHS(u, 1);
+            end
+        end
+       
+        function J = Jli(this, problem, u)
+            switch(this.mode)
+                case 1
+                    J = problem.J(u, 1);
+                case 2
+                    J = problem.J(u, 2);
             end  
         end
         
         function J = Jexp(this, problem, u)
             switch(this.mode)
                 case 1
-                    J = problem.J(u, 1);
-                case 2
                     J = problem.J(u, 2);
+                case 2
+                    J = problem.J(u, 1);
             end
-        end
-        
-        function J = Jli(this, problem, u)
-            switch(this.mode)
-                case 1
-                    J = problem.J(u, 2);
-                case 2
-                    J = problem.J(u, 1);
-            end  
         end
         
     end
